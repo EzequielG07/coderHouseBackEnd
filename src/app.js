64918b214f326d0b8f0adb807f2b282ba17a56fb
Ezequiel Guaymas <ezequiel.guaymas07@gmail.com>
2023-06-19 21:51:44 -0300

    Entregable 9

--- src/app.js
+++ src/app.js
@@ -7,21 +7,13 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import apiRouter from './routes/api.router.js';
 import { errorMiddleware } from './middlewares/error.middleware.js';
-<<<<<<< HEAD
 import { logger } from './utils/logger.utils.js';
-=======
->>>>>>> eb0f85b0fa781a66615fd5188ce5aaceee93cce0
 
 const app = express();
 const NODE_ENV = config.node_env
 const log = logger(NODE_ENV);
 app.use(compression());
 
-<<<<<<< HEAD
-=======
-app.use(compression());
-
->>>>>>> eb0f85b0fa781a66615fd5188ce5aaceee93cce0
 // app.use(compression({
 //     brotli: {
 //         enabled: true, zlib: {
@@ -31,7 +23,6 @@ app.use(compression());
 // }));
 
 connectDB();
-<<<<<<< HEAD
 
 app.use(morgan('dev'));
 
@@ -39,14 +30,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-=======
-
-app.use(morgan('dev'));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
->>>>>>> eb0f85b0fa781a66615fd5188ce5aaceee93cce0
 
 app.use('/api', apiRouter);
 
@@ -55,10 +38,6 @@ app.use(errorMiddleware);
 const PORT = config.port;
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
     //console.log(`Server running on port ${PORT}`);
     log.info(`Server running on port ${PORT}`);
-=======
-    console.log(`Server running on port ${PORT}`);
->>>>>>> eb0f85b0fa781a66615fd5188ce5aaceee93cce0
 });
